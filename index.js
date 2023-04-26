db345dec5b03668bbe64adc547c941147c6347c5
1331felipe <felipehuh2023@gmail.com>
2023-04-26 16:50:22 -0300

    upload operadores

---
+++ index.js
@@ -0,0 +1,65 @@
+//const bool1 = true
+//const bool2 = false
+//const bool3 = !bool2
+
+//let resultado = bool1 && bool2
+//console.log("a. ", resultado)
+
+//resultado = bool1 && bool2 && bool3 
+//console.log("b. ", resultado) 
+
+//resultado = !resultado && (bool1 || bool2) 
+//console.log("c. ", resultado)
+
+//console.log("d. ", typeof resultado)
+
+
+//let primeiroNumero = Number(prompt("Digite um numero!"))
+//let segundoNumero = Number(prompt("Digite outro numero!"))
+
+//const soma = primeiroNumero + segundoNumero
+ //const soma = primeiroNumero +segundoNumero
+ 
+//const idade = Number(prompt("digite sua idade"))
+//const idadeMelhorAmigo = Number(prompt("Idade melhor amigo"))
+
+//console.log(idade > - idadeMelhorAmigo);
+
+//const diferenca = idade - idadeMelhorAmigo
+//console.log(idade - idadeMelhorAmigo)
+
+//const numeroPar = Number(prompt("digite um numero par"))
+ 
+//console.log(numeroPar % 2 === 0);
+
+//const idade = Number(prompt("digite sua idade"))
+
+//const idadeEmMeses = * 12
+//const idadeEmDias = * 345
+//const idadeEmHoras = idade * 24
+
+//const numero1 = Number(prompt("digite um numero"))
+//const numero2 = Number(prompt(digite outro numero))
+
+//console.log(numero1 > numero2);
+//console.log(numero1 === numero2);
+//console.log(numero1 % numero2 ===0);
+//console.log(numero2 % numero21 ==);
+
+ let kelvin;
+ let fahrenheit;
+
+ kelvin = (77 - 32)*(5/9)+ 273.15
+ console.log("o valorem fahrenheit de 77")
+
+ 
+
+
+
+
+
+
+
+
+
+
